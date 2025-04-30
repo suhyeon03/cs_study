9d17fe2c7f0ef10aa2c14ad968d658bfd0de1725
suhyeon03 <sjfkdskfkd89@naver.com>
2025-04-30 19:39:14 +0900

    Title: 문자열 정렬, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250430/문자열 정렬/string-sort.py
+++ 250430/문자열 정렬/string-sort.py
@@ -2,7 +2,10 @@ str = input()
 
 # Please write your code here.
 
+# sorted_arr = sorted(str)
+# sorted_str = ''.join(sorted_arr)
 str_1 = list(str)
 str_1.sort()
+# str_1.sort()
 for i in str_1:
     print(i, end='')
